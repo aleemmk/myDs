c54734830f080c7e1795db1479aad088281913c0
aleemmk <46021226+aleemmk@users.noreply.github.com>
2022-06-26 13:13:25 +0530

    Time: 9 ms (16.69%), Space: 45.3 MB (5.37%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.java
+++ 496-next-greater-element-i/496-next-greater-element-i.java
@@ -1,9 +1,17 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
       /**
-         [1,3,4,2]
-         3 4 -1 -1
-         calculate next greater nge arr[]
+         [1,3, 4, 2]
+          3 4 -1 -1
+         calculate next greater nge arr[] .
+         more intutive way is to iterate from right to left , pop all element from statck untill 
+         we get an element less the the current arr[i] .
+         
+         if stack is empty means no element is grater than arr[i] put -1 in result.
+         else current top of stack is next greater element store in result.
+         
+         use hash map to store elemt as key and next greater as value.
+         
          */
         int n = nums2.length;
         int [] nge = new int[n];
@@ -16,7 +24,7 @@ class Solution {
             while(!stack.isEmpty() && stack.peek() <= nums2[i]){
                 stack.pop();
             }
-            if(i < n){
+         
                 if(!stack.isEmpty()){
                     nge[i]=stack.peek();
                     map.put(nums2[i],nge[i]);
@@ -24,7 +32,7 @@ class Solution {
                     nge[i] =-1;
                     map.put(nums2[i],-1);
                 }
-            }
+            
             stack.push(nums2[i]);
         }
         System.out.println(Arrays.toString(nge));
