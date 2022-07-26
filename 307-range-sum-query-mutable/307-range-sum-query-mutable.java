da74a168c6c1a0042c119a27435f038745b75345
aleemmk <46021226+aleemmk@users.noreply.github.com>
2022-07-26 09:35:38 +0530

    Time: 166 ms (32.39%), Space: 137.2 MB (11.81%) - LeetHub

--- 307-range-sum-query-mutable/307-range-sum-query-mutable.java
+++ 307-range-sum-query-mutable/307-range-sum-query-mutable.java
@@ -1,9 +1,30 @@
+/**
+  Range sum query mutable (with update);
+   we can slve this segment tree 
+   segment tree can be represented by array . total number of node in segment tree always 
+    <=  4N N is the element in the array. proof is below
+    
+    https://www.quora.com/Why-does-4-*-N-space-have-to-be-allocated-for-a-segment-tree-where-N-is-the-size-of-the-original-array
+    
+    left node of ST 2*i
+    right node of ST 2*i+1
+    
+    i start with 1
+    
+    we partion the array from mid and keep of doing until single element remins (start== end)
+    
+    each node value of ST is equal to sum of (left node + right node). we do this recursively.
+    
+   
+*/
+
+
 class NumArray {
        
     int st [];
     int st_idx;
     int start;
-        int end;
+    int end;
     
     public NumArray(int[] nums) {
         int n = nums.length;
@@ -18,11 +39,12 @@ class NumArray {
         
         if(start > end ) return ;
            
+        // leaf node of segment tree
             if(start == end) {
                 st[st_idx] = nums[start];
                 return ;
             }
-        // INTERNAL NODE
+        // INTERNAL NODE of segmant 
         int mid = start+(end-start)/2;
         
         buidSegTree(2*st_idx,nums,start,mid);
@@ -38,7 +60,7 @@ class NumArray {
     
     private void updateNode(int st_idx,int start,int end,int pos,int newVal ){
     
-        if(start > pos || end < pos) return ;
+        if(start > pos || end < pos) return ;  // pos lies outside the boundry
         if(start == end){
             st[st_idx] = newVal;
             return ;
@@ -56,12 +78,26 @@ class NumArray {
         return query(left,right,st_idx,start,end);
     }
     private int query(int qs,int qe,int st_idx,int start,int end){
+        
+        // no overlap
         if( qs > end || qe < start) return 0;
         
+        /* total overlap when node range totally lies in query range
+            
+        1. qs             qe        (node range in between query range)
+               start end
+            
+          2.   qs          qe      (node range in starts with query range and end before query range)
+               start  end
+             
+            3.  qs          qe      (node range in starts inbetween query range and end at query range)
+                    start  end
+         **/  
         if(start >= qs && end <= qe)
             return st[st_idx];
         
-        // partial overlap
+        // partial overlap  
+        
         int mid = start+(end-start)/2;
         int lsum = query(qs,qe,2*st_idx,start,mid);
         int rsum = query(qs,qe,2*st_idx+1,mid+1,end);
