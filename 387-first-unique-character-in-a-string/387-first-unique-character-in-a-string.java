40f493cccfbce10446aebac73987fe4072fa0d0b
aleemmk <46021226+aleemmk@users.noreply.github.com>
2022-08-29 11:37:02 +0530

    Time: 18 ms (59.46%), Space: 48 MB (80.69%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -11,15 +11,15 @@ class Solution {
     */
     public int firstUniqChar(String s) {
         
-        int count [] = new int [256];
+        int count [] = new int [26];
         
         for(int i=0;i<s.length();i++){
-            count[s.charAt(i)]++;
+            count[s.charAt(i)-'a']++;
         }
         
         for(int i=0;i<s.length();i++){
             
-            if(count[s.charAt(i)] == 1)
+            if(count[s.charAt(i)-'a'] == 1)
                 return i;
         }
         return -1;
