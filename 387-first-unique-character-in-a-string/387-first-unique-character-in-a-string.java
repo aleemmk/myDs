b909b4bb7e26316eac2ab392dc46f9b5f019a511
aleemmk <46021226+aleemmk@users.noreply.github.com>
2022-08-29 12:22:00 +0530

    Time: 17 ms (59.72%), Space: 48.5 MB (68.44%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.java
@@ -9,7 +9,32 @@ class Solution {
     maintin character count array of string.
     return first character from left index whose count is 1.
     */
-    public int firstUniqChar(String s) {
+    
+     public int firstUniqChar(String s) {
+        
+        int count [] = new int [26];
+         
+         Arrays.fill(count,-1);
+       
+        for(int i=0;i<s.length();i++){
+            
+            if(count[s.charAt(i)-'a'] == -1){
+                 count[s.charAt(i)-'a'] = i;  // putting unique character index occurs in input string 
+            }else{
+                 count[s.charAt(i)-'a'] = -2;
+            }
+           
+        }
+        int minIndex = Integer.MAX_VALUE;
+        for(int i=0;i<26;i++){
+            
+            if(count[i] >= 0)
+                minIndex = Math.min(count[i],minIndex);   // choosing min index occurs
+        }
+        return minIndex ==Integer.MAX_VALUE ?-1:minIndex ;
+    }
+    
+    public int firstUniqChar1(String s) {
         
         int count [] = new int [26];
         
