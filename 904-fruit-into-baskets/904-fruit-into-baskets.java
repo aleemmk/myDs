d873749a302b4601b405bdb8a8fbf16de132ccb6
aleemmk <46021226+aleemmk@users.noreply.github.com>
2022-09-02 13:51:12 +0530

    Time: 116 ms (24.44%), Space: 113.3 MB (23.00%) - LeetHub

--- 904-fruit-into-baskets/904-fruit-into-baskets.java
+++ 904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -2,8 +2,19 @@ class Solution {
     
     
     /**
-     
-     [3,]
+    j=0 
+     [3,3,3,1,2,1,1,2,3,3, 4 ]
+  i=  0 1 2 3 4 5 6 7 8 9 10
+  
+  putting into map keep track of window i-j+1 ,
+  3-> 1+1+1
+  1-> 1
+  2->1
+  as soon as map size is > 2 we decrese count of fruit at j index and move j
+  keep doing this until size is greater than 2 
+  we keep on moving j and decrease 3 key count as soon as it reach zero remove the key and size of map is 2
+  
+  
     */
      public int totalFruit(int[] fruits) {
          
@@ -17,67 +28,20 @@ class Solution {
              
              map.put(fruits[i],map.getOrDefault(fruits[i],0) +1);
              
-             while(map.size() > 2){
-                  map.put(fruits[j],map.get(fruits[j])-1);
-                 if(map.get(fruits[j]) == 0)
-                     map.remove(fruits[j]);
+             while(map.size() > 2){   // keep on doing untill size is 2
+                  map.put(fruits[j],map.get(fruits[j])-1);   // reduce fruits[j] count and move j
+                
+                 if(map.get(fruits[j]) == 0)  // as soon as fruits againg key is zero remove key to make size 2
+                     map.remove(fruits[j]);  
                  
-                 j++;
+                 j++;   // incrase j to shrink window
             }
              
-             maxsize = Math.max(maxsize,i-j+1);
+             maxsize = Math.max(maxsize,i-j+1);   // keep track of max window size 
             
          }
          
-         return maxsize;
+         return maxsize;  // return maxwindow till now
          
      }
-    public int totalFruit_depricated(int[] fruits) {
-        
-    
-        
-       // basket 1     A   picked, picked
-            
-         //   basket 2    B picked  picked
-        
-        // c Type in between came    stop
-        
-        // Input: fruits = [1,2,3,2,2] 
-        
-        int lastFruit = -1;
-        int secondLastFruit = -1;
-        
-       int max = Integer.MIN_VALUE;
-        
-        int basketFruitCount=0;
-        
-        int lastFruitCount=0;
-        
-        
-        for(Integer currFruit:fruits){
-            
-            if(currFruit == lastFruit || currFruit == secondLastFruit )
-                basketFruitCount++;
-            else
-                basketFruitCount = lastFruitCount+1;
-            
-            if(currFruit == lastFruit){
-                lastFruitCount = lastFruitCount+1;
-            }else{
-                lastFruitCount=1;
-            }
-            
-            
-            if(currFruit != lastFruit){
-                secondLastFruit = lastFruit;
-                lastFruit =currFruit;
-            }
-                
-           
-            max = Math.max(basketFruitCount,max);
-            
-        }
-        
-        return max;
-    }
 }
