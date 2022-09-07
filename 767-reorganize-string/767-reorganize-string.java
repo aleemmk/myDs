bf53230f9e893bc3b9b1cc666a8f9da345e36015
aleemmk <46021226+aleemmk@users.noreply.github.com>
2022-09-07 11:10:42 +0530

    Time: 1 ms (97.13%), Space: 42.1 MB (70.82%) - LeetHub

--- 767-reorganize-string/767-reorganize-string.java
+++ 767-reorganize-string/767-reorganize-string.java
@@ -15,7 +15,7 @@ class Solution {
         int count [] = new int [26];
         
         for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
+            count[s.charAt(i)-'a']++;   // frequency count
         }
         int max = 0 ;
         int letter = 0;
@@ -23,21 +23,21 @@ class Solution {
            
             
             if(max < count[i] ){
-                max = count[i];
-                letter = i;
+                max = count[i];   // max count of a char in string
+                letter = i;        //  letter of max count
             }
         }
         
-        if(max > (s.length()+1)/2) return "";
+        if(max > (s.length()+1)/2) return "";    // need to place at even position if max is more than even place then same char must be adjacent
         
-        int idx=0;
+        int idx=0;   // firs teven pos
         char res[]= new char[s.length()];
         
         while(count[letter] > 0){
             
             res[idx] =  (char) (letter +'a');
-            idx +=2;
-            count[letter]--;
+            idx +=2;     // keep on iterating even pos
+            count[letter]--;     // decrese freq in map
         }
         
         
@@ -46,15 +46,15 @@ class Solution {
             
                while(count[i] > 0){
                     
-                    if(idx >= s.length()) idx=1;
+                    if(idx >= s.length()) idx=1;  // if idx ar last position of beyound boundry pick it first                         odd place as it is empty
                    
-                    res[idx] = (char) (i+'a');
-                    idx +=2;
+                    res[idx] = (char) (i+'a');   
+                    idx +=2;    // if even place left pick char when reach end start with 1 index from above
                     count[i]--;
                     
                 }
         }
         
-        return String.valueOf(res);
+        return String.valueOf(res);   // resturn res string from array
     }
 }
