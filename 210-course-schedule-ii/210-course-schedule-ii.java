8e110850d9367810999f63d79c57f118d3b5981e
aleemmk <46021226+aleemmk@users.noreply.github.com>
2022-09-11 23:20:16 +0530

    Time: 10 ms (55.70%), Space: 48.5 MB (74.87%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.java
+++ 210-course-schedule-ii/210-course-schedule-ii.java
@@ -26,6 +26,90 @@ class Solution {
      
     */
     
+    
+    /**
+     Technic 2:
+     
+      kahn's algo
+      
+     BFS 
+     
+     1. filling indegree array
+     
+     2. filling queue ( push all node whose indgree is 0 in queue. we start topological sort from node whose indegree is 0               and keep on doing)
+     
+     3. processing queue. (removing curr node from graph it means indegree of adjacent node is decreasing if it is zero add int to the queue)
+     
+     when curr node processed add in ans list and increaed the processed node count.
+     
+     and finally
+     if(processed node count != total node) means cycle.
+     
+    
+    */
+    
+    
+    
+    /**BFS kahn' algo start */
+     public int[] findOrder(int numCourses, int[][] prerequisites) {
+          List<List<Integer>> adj = new ArrayList<>();
+         
+          for(int i=0;i< numCourses ;i++) adj.add(new ArrayList<>());
+        
+         
+         int indegree[] = new int[numCourses];
+         
+         
+         for(int i=0;i<prerequisites.length;i++){
+            adj.get(prerequisites[i][1]).add(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]  +=1;     // fill indegree array  STEP 1
+        }
+         
+         int[] res = new int[numCourses];
+         int emptyArr [] = {};
+         
+         if(kahnsAlgo(adj,numCourses,indegree,res)) return res;     // if kahan's alogo success 
+         
+         return emptyArr;   // return empty arr if not found
+         
+     }
+    
+    private boolean kahnsAlgo(List<List<Integer>> adj ,int numCourses, int indegree [],int [] res){
+        
+        Queue<Integer> q = new LinkedList<>();
+        int count=0;
+        int index=0;
+        for(int i=0;i<indegree.length;i++){
+            if(indegree[i] == 0)
+                q.add(i);                       // fill queue with indegree 0   STEP 2
+                
+        }
+        
+        while(!q.isEmpty()){
+            
+            int curr = q.poll();                     // process curr node
+            
+            for(int neighbour:adj.get(curr)){
+                
+                indegree[neighbour] -=1;                  // decrease all negibours indegree
+                if(indegree[neighbour] == 0)              // during decrement if it is 0 add to queue
+                    q.offer(neighbour);
+            }
+            
+            res[index++] =curr;                          // add curr into ans when it is processed
+            count +=1 ;                                  // processed node count
+        }
+        
+        if(count != numCourses) return false;         // finally procced node count not equal to total vertices means cycle
+        
+        return true;
+        
+    }
+    
+     /**BFS kahn' algo end */
+    
+    
+    
     private boolean isCycle(List<List<Integer>> adj  ,int numCourses){
          int visited [] = new int[numCourses];
         
@@ -54,7 +138,7 @@ class Solution {
         return false;
     }
     
-    public int[] findOrder(int numCourses, int[][] prerequisites) {
+    public int[] findOrder1(int numCourses, int[][] prerequisites) {
         
         List<List<Integer>> adj = new ArrayList<>();
         int res []  = new int[numCourses];
