9ef2a366f7963af6433303e93d256911c996c334
aleemmk <46021226+aleemmk@users.noreply.github.com>
2022-09-18 00:01:22 +0530

    Time: 8 ms (58.43%), Space: 48.1 MB (78.32%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -8,7 +8,7 @@ class MinStack {
      s2-> to track min value
     */
 
-    Stack<Integer> stack;
+   /* Stack<Integer> stack;
     Stack<Integer> minStack;
     
     public MinStack() {
@@ -38,6 +38,50 @@ class MinStack {
     
     public int getMin() {
        return  minStack.peek();
+    }*/
+    
+    /** 
+       we can use linked list  Node maintains min val;
+    */
+    
+    
+    class Node{
+        int val;
+        int min;
+        Node next;
+        
+        Node(int val,int min,Node next){
+            this.val =  val;
+            this.min =  min;
+            this.next = next;
+        }
+    }
+    
+    Node head;
+    public MinStack() {
+    }
+    
+    public void push(int val) {
+        
+        if(head == null){
+            head  = new Node(val,val,null);
+        }else{
+         head = new Node(val,Math.min(val,head.min),head); //add new ele in as ne head and next is curr head
+      }
+      
+    }
+    
+    public void pop() {
+       head = head.next;
+        
+    }
+    
+    public int top() {
+     return head.val;
+    }
+    
+    public int getMin() {
+       return head.min;
     }
 }
 
